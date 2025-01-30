74264cbe6925ee4c849b8c468a306f593e5ae8e5
Adrian Alvarado <adaz1989@gmail.com>
2025-01-30 15:49:55 -0600

    elimine top baner del layout

--- ProyectoDeportivoCR/obj/Debug/net8.0/ProyectoDeportivoCR.AssemblyInfo.cs
+++ ProyectoDeportivoCR/obj/Debug/net8.0/ProyectoDeportivoCR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoDeportivoCR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c6904d1eaf335ff2aba868f95dbc4f7dd59e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6ccd085cf90e2b2d2af94c41488368ae0cbeb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoDeportivoCR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoDeportivoCR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
